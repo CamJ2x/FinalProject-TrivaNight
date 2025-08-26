298fbb90ce9076041daf9aed4b4c108d75f7269f
Cameron Johnson <cjohnson22@msu.idserve.net>
2025-08-25 20:00:33 -0400

    backend of API

--- Trivia/src/app/app.ts
+++ Trivia/src/app/app.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TriviaInfoLayout } from './interfaces/trivia-info-layout';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TriviaApi } from './services/trivia-api';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected title = 'Trivia';
+
+  export class App implements OnInit {
+
+  protected title = 'Trivia Night ';
+
+  constructor(private triviaApi: TriviaApi) {}
+
+
+  theMovies : any[] = [];
+
+
+  newTrivia : TriviaInfoLayout = {
+                                      id: 0,
+                                      title : "",
+                                      year : "",
+                                      genre: "",
+                                      actors : "",
+                                      plot : ""
+                                   }
+
+  async ngOnInit() 
+  {
+  this.theMovies = await this.triviaApi.getMovie();
+  }
+  
+  async addMovie(newMovie : TriviaInfoLayout) {
+   
+    await this.triviaApi.addMovie(this.newTrivia)
+  
+    this.theMovies = await this.triviaApi.getMovie()
+  }
+  
+ 
+
 }
---
+++ Trivia/src/app/components/homepage/homepage.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { TriviaApi } from '../../services/trivia-api';
+
+@Component({
+  selector: 'app-homepage',
+  imports: [],
+  templateUrl: './homepage.html',
+  styleUrl: './homepage.css'
+})
+export class Homepage {
+  
+}
+
+
