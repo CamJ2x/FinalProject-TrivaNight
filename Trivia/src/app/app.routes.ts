f4ceca2fd887ead7a9f606670c77ee35a8a194b3
ebast1 <ebast23@msu.idserve.net>
2025-09-04 19:21:03 -0400

    added path to home on default

--- Trivia/src/app/app.routes.ts
+++ Trivia/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Verusgame } from './components/verusgame/verusgame';
 import { Homepage } from './components/homepage/homepage';
 
 export const routes: Routes = [
+    {path: '',   redirectTo: 'home', pathMatch: 'full'},
     {path: 'home',       component: Homepage},
     {path: 'soloGame',   component: Sologame}, 
     {path: 'versusGame', component: Verusgame}
