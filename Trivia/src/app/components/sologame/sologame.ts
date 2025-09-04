59b184f6abf2a02d43b66599f214a572742e0f53
Cameron Johnson <cjohnson22@msu.idserve.net>
2025-09-04 19:28:03 -0400

    fixing merge issues

--- Trivia/src/app/components/sologame/sologame.ts
+++ Trivia/src/app/components/sologame/sologame.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-sologame',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './sologame.html',
   styleUrl: './sologame.css'
 })
 export class Sologame {
-
+  pageTitle = " Solo Play"
 }
