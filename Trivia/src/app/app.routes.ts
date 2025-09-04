c0c646c0e0563710f767672dce7fdedec2330469
ebast1 <ebast23@msu.idserve.net>
2025-09-04 19:12:28 -0400

    deleted home path in routes

--- Trivia/src/app/app.routes.ts
+++ Trivia/src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { Verusgame } from './components/verusgame/verusgame';
 import { Homepage } from './components/homepage/homepage';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home',       component: Homepage},
     {path: 'soloGame',   component: Sologame}, 
     {path: 'versusGame', component: Verusgame}
