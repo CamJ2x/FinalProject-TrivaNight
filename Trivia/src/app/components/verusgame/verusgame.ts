0ac4b7ba15f4b77b60beaa4272883eb584e0f911
Cameron Johnson <cjohnson22@msu.idserve.net>
2025-09-08 17:15:30 -0400

    updates to player screen

--- Trivia/src/app/components/verusgame/verusgame.ts
+++ Trivia/src/app/components/verusgame/verusgame.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-verusgame',
-  imports: [],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './verusgame.html',
   styleUrl: './verusgame.css'
 })
 export class Verusgame {
+
+  nextQuestion() {
+    
+  }
   
 
 }
