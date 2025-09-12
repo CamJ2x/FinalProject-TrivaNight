65cb2b97c95782a9250bb0dd416c8f3f03349c2d
Darryl Bentley <darrylbentley582@gmail.com>
2025-09-11 20:58:08 -0400

    Thurs

--- Trivia/src/app/components/sologame/sologame.ts
+++ Trivia/src/app/components/sologame/sologame.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
+import { LeaderboardComponent } from '../leaderboard/leaderboard';
 import { TriviaApi } from '../../services/trivia-api';
 
 @Component({
   selector: 'app-sologame',
-  imports: [],
+  standalone: true,                 
+  imports: [LeaderboardComponent],
   templateUrl: './sologame.html',
-  styleUrl: './sologame.css'
+  styleUrls: ['./sologame.css']      
 })
 export class Sologame {
 
-  constructor(private triviaApi: TriviaApi) {}
-
   movie: any;
 
+  leaderboard = [
+    { name: 'Alice', score: 120 },
+    { name: 'Bob', score: 95 },
+    { name: 'Charlie', score: 75 },
+  ];
+
+  constructor(private triviaApi: TriviaApi) {}
+
   testApi(): void {
-     // FOR NOW YOU MANUALLY TYPE IN THE TITLE OF THE MOVIE YOU WANT TO HAVE POPULATE THE INFORMATION FIELD INSIDE THE SOLOGAME.html
     this.triviaApi.searchMovie("A Man Called Otto")
       .then((data: any) => {
         this.movie = data;
@@ -27,5 +34,4 @@ export class Sologame {
   ngOnInit(): void {
     // Something :)
   }
-
 }
