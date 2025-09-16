d672f99da88f86f506794032dfb8c28c35d7942f
Darryl Bentley <darrylbentley582@gmail.com>
2025-09-15 21:53:03 -0400

    Mon

--- Trivia/src/app/components/sologame/sologame.ts
+++ Trivia/src/app/components/sologame/sologame.ts
@@ -1,28 +1,18 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import { LeaderboardComponent } from '../leaderboard/leaderboard';
-=======
-import { GameDataService } from '../../services/game-data-service';
-import { OnInit } from '@angular/core';
->>>>>>> 19fa0d6f65cf500bb34f0bbcef3ba26318ced256
 import { TriviaApi } from '../../services/trivia-api';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sologame',
-<<<<<<< HEAD
   standalone: true,                 
   imports: [LeaderboardComponent],
-=======
-  imports: [CommonModule],
->>>>>>> 19fa0d6f65cf500bb34f0bbcef3ba26318ced256
   templateUrl: './sologame.html',
   styleUrls: ['./sologame.css']      
 })
 export class Sologame implements OnInit {
 
-<<<<<<< HEAD
   movie: any;
 
   leaderboard = [
@@ -36,28 +26,6 @@ export class Sologame implements OnInit {
   testApi(): void {
      // FOR NOW YOU MANUALLY TYPE IN THE TITLE OF THE MOVIE YOU WANT TO HAVE POPULATE THE INFORMATION FIELD INSIDE THE SOLOGAME.html
     this.triviaApi.searchMovie("A Man Called Otto")
-=======
-  constructor(public gameData: GameDataService, public triviaApi: TriviaApi, private router: Router) {}
-
-  movie: any = null;
-
-  reveal: boolean = false;
-
-  testApi(): void {
-       // Example of fetching by title
-
-    // this.triviaApi.searchMovie("A Man Called Otto")
-    //   .then((data: any) => {
-    //     this.movie = data;
-    //   })
-    //   .catch((err: any) => {
-    //     console.error('Error fetching movie:', err);
-    //   });
-
-      // Example of fetching by ID
-
-      this.triviaApi.getMovieById("tt0458339")
->>>>>>> 19fa0d6f65cf500bb34f0bbcef3ba26318ced256
       .then((data: any) => {
         this.movie = data;
         console.log("Movie fetched by ID:", data);
